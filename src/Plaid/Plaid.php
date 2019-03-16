5bf763996de093bba4b08cfca7730acf1bf2390d
cameron macfarlane <cammac1984@gmail.com>
2019-03-15 22:58:46 -0700

    testing updates, debugging

--- src/Plaid/Plaid.php
+++ src/Plaid/Plaid.php
@@ -2,7 +2,11 @@
 
 namespace Jameron\Plaid;
 
-use Illuminate\Http\Request;
+use Carbon\Carbon;
+use GuzzleHttp\Client as Guzzle;
+use GuzzleHttp\Post\PostBodyInterface;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\RequestException;
 
 class Plaid
 {
@@ -11,10 +15,47 @@ class Plaid
      *
      * @var array
      */
-
     protected $config;
 
-    public static function test() {
-        return 'test';
+    public static function test($username, $password, $institution) {
+        return self::addAuthUser($username, $password,null, $institution);
+    }
+
+    /**
+     * Initialize the Guzzle Client and make it ready for all requests
+     */
+    public static function client()
+    {
+        return new Guzzle(['base_uri' => config('plaid2.api_host')]);
+    }
+
+    /**
+     * [addAuthUser description]
+     * @method addAuthUser
+     * @param  [string]      $username [description]
+     * @param  [string]      $password [description]
+     * @param  [string]      $pin      [description]
+     * @param  [string]      $institutionId     [description]
+     */
+    public static function addAuthUser($username, $password, $pin = null, $institutionId)
+    {
+        try {
+            $request = self::client()->post('auth', [
+                'json' => [
+                    'client_id' => config('plaid2.client_id'),
+                    'secret' => config('plaid2.secret'),
+                    'username' => $username,
+                    'password' => $password,
+                    'pin' => $pin,
+                    'type' => $institutionId,
+                    'options' => [
+                        'list' => config('plaid.auth.list')
+                    ]
+                ]
+            ]);
+            return json_decode($request->getBody(), true);
+        } catch (RequestException $e) {
+            return json_decode($e->getResponse()->getBody()->getContents(), true);
+        }
     }
 }
