aede9692667a44b93ac5353f1c5c0567d01f17b1
Cameron Macfarlane <cameron@nursegrid.com>
2020-11-22 21:02:10 -0800

    testing updates, debugging

--- src/config/plaid.php
+++ src/config/plaid.php
@@ -1,6 +1,8 @@
 <?php
 return [
-    'api_host'  => 'https://development.plaid.com/',
-    'client_id' => env('PLAID_CLIENT_ID'),
-    'secret'    => env('PLAID_SECRET')
+    'api_host'   => 'https://development.plaid.com/',
+    'client_id'  => env('PLAID_CLIENT_ID'),
+    'secret'     => env('PLAID_SECRET'),
+    'clientName' => env('PLAID_CLIENT_NAME'),
+    'webhook'    => env('PLAID_WEBHOOK')
 ];
